e4816dc82dbd334b068221d6f83e4e4ce7cbbf64
子風 <zpsyhapcst@gmail.com>
2018-03-03 16:39:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,14 @@ setuptools config
 from setuptools import setup
 
 setup(
-    name='pcf',
-    packages=['pcf'],
+    name='flaskr',
+    packages=['flaskr'],
     include_package_data=True,
     install_requires=[
         'flask',
         'flask_script',
         'flask_sqlalchemy',
-		'flask_admin',
+	'flask_admin',
         'flask_login',
-        'pandas',
-        'pyquery',
-        'requests',
-        'xlrd',
     ],
 )
